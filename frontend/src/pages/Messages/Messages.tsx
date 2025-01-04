9aebdfef2a0bfc17c3ab46165d13fd688fedbbff
007killer2 <savenko.artem@gmx.de>
2025-01-04 18:50:38 +0100

    Message Chat Implementation

--- frontend/src/pages/Messages/Messages.tsx
+++ frontend/src/pages/Messages/Messages.tsx
@@ -33,7 +33,87 @@ export const Messages = () => {
                     <h1>nikita</h1>
                 </div>
 
-                <div>
+                <div className={styles.chat_container}>
+                    <div className={styles.chat_interlocutor}>
+                        <img src={nikita_avatar} alt=""/>
+                        <h3>Nikita</h3>
+                        <div className={styles.chat_interlocutor_details}>
+                            <span>Nikita.</span>
+                            <span>ICHgram</span>
+                        </div>
+                        <button className={styles.view_profile_btn}>View Profile</button>
+                    </div>
+                    <h4>Jun 26, 2024, 08:49 PM.</h4>
+
+                    <div className={styles.chat_history}>
+                        <div className={styles.message_of_interlocutor}>
+                            <img src={nikita_avatar} alt=""/>
+                            <div className={styles.message_of_interlocutor_text}>
+                                <p>Hey, how are you?</p>
+                                <span>2 wek</span>
+                            </div>
+                        </div>
+
+                        <div className={styles.my_message}>
+                            <div className={styles.my_message_text}>
+                                <p>Hey, I'm fine. How about you?</p>
+                                <span>2 wek</span>
+                            </div>
+                            <img src={sasha_avatar} alt=""/>
+                        </div>
+
+
+                        <div className={styles.message_of_interlocutor}>
+                            <img src={nikita_avatar} alt=""/>
+                            <div className={styles.message_of_interlocutor_text}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.</p>
+                                <span>2 wek</span>
+                            </div>
+                        </div>
+
+                        <div className={styles.my_message}>
+                            <div className={styles.my_message_text}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.</p>
+                                <span>2 wek</span>
+                            </div>
+                            <img src={sasha_avatar} alt=""/>
+                        </div>
+
+
+                        <div className={styles.message_of_interlocutor}>
+                            <img src={nikita_avatar} alt=""/>
+                            <div className={styles.message_of_interlocutor_text}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.</p>
+                                <span>2 wek</span>
+                            </div>
+                        </div>
+
+                        <div className={styles.message_of_interlocutor}>
+                            <img src={nikita_avatar} alt=""/>
+                            <div className={styles.message_of_interlocutor_text}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <span>2 wek</span>
+                            </div>
+                        </div>
+
+                        <div className={styles.my_message}>
+                            <div className={styles.my_message_text}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.</p>
+                                <span>2 wek</span>
+                            </div>
+                            <img src={sasha_avatar} alt=""/>
+                        </div>
+
+
+                    </div>
+
+                    <div className={styles.chat_input_container}>
+                        <input className={styles.chat_input} type="text" placeholder="Write message"/>
+                    </div>
 
                 </div>
             </div>
