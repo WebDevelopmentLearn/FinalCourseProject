93d8e1e167b0d1f5aaf3929ca316a2169e955841
007killer2 <savenko.artem@gmx.de>
2025-01-26 21:36:05 +0100

    (Backend) Remove multer debug

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -7,18 +7,7 @@ import {MulterError} from "multer";
 const router: Router = Router();
 
 router.get("/profile", checkAccessToken, UserController.getUserProfile);
-router.put("/update-profile",  (req, res, next) => {
-    UploadFiles.uploadSingle("avatar")(req, res, (err) => {
-        if (err instanceof MulterError) {
-            console.error("Multer error:", err.message);
-            return res.status(400).json({ message: err.message });
-        } else if (err) {
-            console.error("Unexpected error:", err.message);
-            return res.status(500).json({ message: "Internal server error" });
-        }
-        next();
-    });
-}, checkAccessToken, UserController.editProfile);
+router.put("/update-profile", UploadFiles.uploadSingle("avatar"), checkAccessToken, UserController.editProfile);
 
 
 
