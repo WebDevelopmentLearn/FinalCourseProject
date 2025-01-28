041b882c17bec069e3de3f2f3176cd4ceef49932
007killer2 <savenko.artem@gmx.de>
2025-01-28 20:00:43 +0100

    (Frontend) Fix AvatarCircle

--- frontend/src/components/other/AvatarCircle/AvatarCircle.tsx
+++ frontend/src/components/other/AvatarCircle/AvatarCircle.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useMemo} from "react";
 import {NavigateFunction, useNavigate} from "react-router-dom";
 
 import {AvatarCircleProps} from "../../../utils/Entitys.ts";
@@ -19,7 +19,15 @@ export const AvatarCircle: FC<AvatarCircleProps> = ({user, className, avatarSize
         }
     }
 
+    const numArray = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    ]
 
+    const randId = useMemo(
+        () => Array.from({ length: 10 }, () => numArray[Math.floor(Math.random() * numArray.length)]).join(''),
+        []
+    );
     if (hasLink) {
         return (
             <div onClick={() => {
@@ -30,18 +38,18 @@ export const AvatarCircle: FC<AvatarCircleProps> = ({user, className, avatarSize
                          xmlns="http://www.w3.org/2000/svg">
 
                         <defs>
-                            <linearGradient id={`gradient-${avatarSize}`} x1="0%" y1="0%" x2="100%" y2="100%">
+                            <linearGradient id={`gradient-${avatarSize}-${randId}`} x1="0%" y1="0%" x2="100%" y2="100%">
                                 <stop offset="0%" stopColor="#ff0000"/>
                                 <stop offset="50%" stopColor="#ff00ff"/>
                                 <stop offset="100%" stopColor="#ffff00"/>
                             </linearGradient>
-                            <clipPath id={`circleClip-${avatarSize}`}>
+                            <clipPath id={`circleClip-${avatarSize}-${randId}`}>
                                 <circle cx="100" cy="100" r="80"/>
                             </clipPath>
                         </defs>
 
 
-                        <circle cx="100" cy="100" r="90" stroke={`url(#gradient-${avatarSize})`} strokeWidth="10" fill="none"/>
+                        <circle cx="100" cy="100" r="90" stroke={`url(#gradient-${avatarSize}-${randId})`} strokeWidth="10" fill="none"/>
 
 
                         <circle cx="100" cy="100" r="80" fill="#ffffff"/>
@@ -50,7 +58,7 @@ export const AvatarCircle: FC<AvatarCircleProps> = ({user, className, avatarSize
 
                                x="0" y="0"
                                width="100%" height="100%"
-                               clipPath={`url(#circleClip-${avatarSize})`}
+                               clipPath={`url(#circleClip-${avatarSize}-${randId})`}
                         />
                     </svg>
                 ) : (
@@ -66,18 +74,18 @@ export const AvatarCircle: FC<AvatarCircleProps> = ({user, className, avatarSize
                      xmlns="http://www.w3.org/2000/svg">
 
                     <defs>
-                        <linearGradient id={`gradient-${avatarSize}`} x1="0%" y1="0%" x2="100%" y2="100%">
+                        <linearGradient id={`gradient-${avatarSize}-${randId}`} x1="0%" y1="0%" x2="100%" y2="100%">
                             <stop offset="0%" stopColor="#ff0000"/>
                             <stop offset="50%" stopColor="#ff00ff"/>
                             <stop offset="100%" stopColor="#ffff00"/>
                         </linearGradient>
-                        <clipPath id={`circleClip-${avatarSize}`}>
+                        <clipPath id={`circleClip-${avatarSize}-${randId}`}>
                             <circle cx="100" cy="100" r="80"/>
                         </clipPath>
                     </defs>
 
 
-                    <circle cx="100" cy="100" r="90" stroke={`url(#gradient-${avatarSize})`} strokeWidth="10" fill="none"/>
+                    <circle cx="100" cy="100" r="90" stroke={`url(#gradient-${avatarSize}-${randId})`} strokeWidth="10" fill="none"/>
 
 
                     <circle cx="100" cy="100" r="80" fill="#ffffff"/>
@@ -86,7 +94,7 @@ export const AvatarCircle: FC<AvatarCircleProps> = ({user, className, avatarSize
 
                            x="0" y="0"
                            width="100%" height="100%"
-                           clipPath={`url(#circleClip-${avatarSize})`}
+                           clipPath={`url(#circleClip-${avatarSize}-${randId})`}
                     />
                 </svg>
             ) : (
