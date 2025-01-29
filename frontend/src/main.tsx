51d2ed9ee2675fcf6a23ff3b91ba46b57d38e19a
007killer2 <savenko.artem@gmx.de>
2025-01-29 14:24:43 +0100

    (Frontend) Change index.html title

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,6 @@ import {ComponentPreviews, useInitial} from "./dev";
 import {ThemeProvider} from "./context/ThemeContext.tsx";
 import ichgramStore from "./store/ichgramStore.ts";
 import {ImageProvider} from "./context/ImageContext.tsx";
-import {ToastContainer} from "react-toastify";
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={ichgramStore}>
